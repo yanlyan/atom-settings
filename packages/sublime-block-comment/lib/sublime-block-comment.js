c755bb5147a0ac88dfa6569b3dbf192dcda3c473
Lyan Dwi Pangestu <gloriancrack@gmail.com>
2015-09-28 18:50:29 +0700

    no message

--- packages/sublime-block-comment/lib/sublime-block-comment.js
+++ packages/sublime-block-comment/lib/sublime-block-comment.js
@@ -1,90 +1,123 @@
 'use babel';
 
-export default {
-  activate() {
-    atom.commands.add('atom-text-editor:not([mini])', { 'sublime-block-comment:toggle': this.toggle });
-  },
-  toggle() {
-    const editor = atom.workspace.getActiveTextEditor();
-    if (!editor) return;
+import core from 'core-js/library';
+const { find } = core.Array.prototype;
 
-    const buffer = editor.getBuffer();
+const disposables = [];
 
-    buffer.transact(() => {
-      for (const selection of editor.getSelectionsOrderedByBufferPosition().reverse()) {
-        const range = selection.getBufferRange();
-        const textInRange = buffer.getTextInRange(range);
+const Lang = (rLangScope, openCommentToken, closeCommentToken) => ({
+  test: (scope) => rLangScope.test(scope),
+  commentTokens: { open: openCommentToken, close: closeCommentToken },
+});
+const langs = [
+  Lang(/^source\.js\.?/ , '/*'  , '*/' ),
+  Lang(/^source\.css\.?/, '/*'  , '*/' ),
+  Lang(/^text\.html\.?/ , '<!--', '-->'),
+];
 
-        // TODO change comment syntax based on scope's grammar
-        const commentTokens = {
-          open:  '/*',
-          close: '*/',
-        };
+export function activate() {
+  disposables.push(atom.commands.add('atom-text-editor:not([mini])', { 'sublime-block-comment:toggle': toggle }));
+}
 
-        // If we are inside a block comment or have one fully or partially selected, uncomment it.
-        if ((isInsideBlockComment(range.start) || isAtCloseCommentToken(range.start, commentTokens.close))
-          && (isInsideBlockComment(range.end) || isAtCloseCommentToken(range.end, commentTokens.close))
-          && ~[-1, textInRange.length - commentTokens.close.length].indexOf(textInRange.indexOf(commentTokens.close))
-        ) {
-          let blockCommentCloseRange;
-          const lastRow = buffer.getLastRow();
-          for (let row = range.end.row; !blockCommentCloseRange && row <= lastRow; row++) {
-            const line = buffer.lineForRow(row);
-            for (let column = row === range.end.row ? Math.max(range.end.column - commentTokens.close.length, 0) : 0; (column = line.indexOf(commentTokens.close, column)) !== -1; column++) {
-              if (isCommentDefinitionPunctuation([row, column])) {
-                blockCommentCloseRange = [[row, column], [row, column + commentTokens.close.length]];
-                break;
-              }
+export function deactivate() {
+  for (const disposable of disposables) disposable.dispose();
+  disposables.length = 0;
+}
+
+function toggle() {
+  const editor = atom.workspace.getActiveTextEditor();
+  if (!editor) return;
+
+  const buffer = editor.getBuffer();
+
+  buffer.transact(() => {
+    for (const selection of editor.getSelectionsOrderedByBufferPosition().reverse()) {
+      const range = selection.getBufferRange();
+      const textInRange = buffer.getTextInRange(range);
+      const scopes = getPositionScopes(range.start).reverse();
+
+      let lang;
+      if (!scopes.some((scope) => lang = langs::find((lang) => lang.test(scope)))) lang = langs[0];
+      const commentTokens = lang.commentTokens;
+
+      // If we are inside a block comment or have one fully or partially selected, uncomment it.
+      if (
+        (isInsideBlockComment(range.start) || isAtCloseCommentToken(range.start, commentTokens.close))
+        && (isInsideBlockComment(range.end) || isAtCloseCommentToken(range.end, commentTokens.close))
+        && ~[-1, textInRange.length - commentTokens.close.length].indexOf(textInRange.indexOf(commentTokens.close))
+      ) {
+        let blockCommentCloseRange;
+        const lastRow = buffer.getLastRow();
+        for (let row = range.end.row; !blockCommentCloseRange && row <= lastRow; row++) {
+          const line = buffer.lineForRow(row);
+          for (
+            let column = row === range.end.row ? Math.max(range.end.column - commentTokens.close.length, 0) : 0;
+            (column = line.indexOf(commentTokens.close, column)) !== -1;
+            column++
+          ) {
+            if (isCommentDefinitionPunctuation([row, column])) {
+              blockCommentCloseRange = [[row, column], [row, column + commentTokens.close.length]];
+              break;
             }
           }
-
-          let blockCommentOpenRange;
-          for (let row = range.start.row; !blockCommentOpenRange && row >= 0; row--) {
-            const line = buffer.lineForRow(row);
-            for (let column = row === range.start.row ? (blockCommentCloseRange && row === blockCommentCloseRange[0][0] ? blockCommentCloseRange[0][1] : Math.min(range.start.column + commentTokens.open.length, line.length)) : line.length; (column = line.lastIndexOf(commentTokens.open, column)) !== -1; column--) {
-              if (isCommentDefinitionPunctuation([row, column])) {
-                blockCommentOpenRange = [[row, column], [row, column + commentTokens.open.length]];
-                break;
-              }
-            }
-          }
-
-          if (blockCommentCloseRange) buffer.delete(blockCommentCloseRange);
-          if (blockCommentOpenRange) buffer.delete(blockCommentOpenRange);
-
-          continue;
         }
 
-        // Else, wrap selection with block comment.
-        buffer.insert(range.end, commentTokens.close);
-        buffer.insert(range.start, commentTokens.open);
+        let blockCommentOpenRange;
+        for (let row = range.start.row; !blockCommentOpenRange && row >= 0; row--) {
+          const line = buffer.lineForRow(row);
+          for (
+            let column = row === range.start.row
+              ? (
+                blockCommentCloseRange && row === blockCommentCloseRange[0][0]
+                  ? blockCommentCloseRange[0][1]
+                  : Math.min(range.start.column + commentTokens.open.length, line.length)
+              )
+              : line.length;
+            (column = line.lastIndexOf(commentTokens.open, column)) !== -1;
+            column--
+          ) {
+            if (isCommentDefinitionPunctuation([row, column])) {
+              blockCommentOpenRange = [[row, column], [row, column + commentTokens.open.length]];
+              break;
+            }
+          }
+        }
 
-        // Unselect the opening and closing comment tokens.
-        var columnOffset = commentTokens.open.length;
-        selection.setBufferRange([
-          [range.start.row, range.start.column + columnOffset],
-          [range.end.row, range.end.column + (range.start.row === range.end.row ? columnOffset : 0)]
-        ]);
+        if (blockCommentCloseRange) buffer.delete(blockCommentCloseRange);
+        if (blockCommentOpenRange) buffer.delete(blockCommentOpenRange);
+
+        continue;
       }
-    });
 
+      // Else, wrap selection with block comment.
+      buffer.insert(range.end, commentTokens.close);
+      buffer.insert(range.start, commentTokens.open);
 
-    function isInsideBlockComment(position) {
-      return getPositionScopes(position).some((scope) => scope.startsWith('comment.block.'));
+      // Unselect the opening and closing comment tokens.
+      const startRowColumnOffset = commentTokens.open.length;
+      selection.setBufferRange([
+        [range.start.row, range.start.column + startRowColumnOffset],
+        [range.end.row, range.end.column + (range.start.row === range.end.row ? startRowColumnOffset : 0)]
+      ]);
     }
+  });
 
-    function isCommentDefinitionPunctuation(position) {
-      return getPositionScopes(position).some((scope) => scope.startsWith('punctuation.definition.comment.'));
-    }
 
-    function getPositionScopes(position) {
-      return editor.scopeDescriptorForBufferPosition(position).getScopesArray();
-    }
+  function isInsideBlockComment(position) {
+    return getPositionScopes(position).some((scope) => scope.startsWith('comment.block.'));
+  }
 
-    function isAtCloseCommentToken(position, closeCommentToken) {
-      return position.column >= closeCommentToken.length
-        && buffer.getTextInRange([[position.row, position.column - closeCommentToken.length], position]) === closeCommentToken
-        && isCommentDefinitionPunctuation([position.row, position.column - 1]);
-    }
-  },
-};
+  function isCommentDefinitionPunctuation(position) {
+    return getPositionScopes(position).some((scope) => scope.startsWith('punctuation.definition.comment.'));
+  }
+
+  function getPositionScopes(position) {
+    return editor.scopeDescriptorForBufferPosition(position).getScopesArray();
+  }
+
+  function isAtCloseCommentToken(position, closeCommentToken) {
+    return position.column >= closeCommentToken.length
+      && buffer.getTextInRange([[position.row, position.column - closeCommentToken.length], position]) === closeCommentToken
+      && isCommentDefinitionPunctuation([position.row, position.column - 1]);
+  }
+}
